5cbd4a290ce0e909aebc0a80a463cc1818416814
tammatYO <95525800+tammatYO@users.noreply.github.com>
2022-01-19 00:35:32 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const {src, dest, watch} = require('gulp');
+const browserSync  = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+
+// static server
+ function bs() {
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
+
+// Compile sass into CSS & auto-inject into browsers
+ function serveSass() {
+    return src("./sass/**/*.sass", "./sass/**/*.scss")
+        .pipe(sass())
+        .pipe(autoprefixer({
+			cascade: false
+		}))
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
